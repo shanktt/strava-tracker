d0481505ceaf338480d819f96d7fa0552b144124
AshankKumar <ashankkumar01@gmail.com>
2024-01-07 16:17:51 -0500

    ditch lambda api

--- components/login-btn.jsx
+++ components/login-btn.jsx
@@ -2,18 +2,10 @@ import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Component() {
   const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.name} <br />
-        <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded" onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
   return (
+    !session ?
     <>
-      Not signed in <br />
       <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded" onClick={() => signIn('strava')}>Sign in with Strava</button>
-    </>
+    </> : null
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import Button from '@/components/login-btn'
 import { StravaData, ApiResponse } from '@/types/types'
-import ActivityCalendar from 'react-activity-calendar';
+import { useSession, signIn, signOut } from "next-auth/react"
+import dynamic from 'next/dynamic';
+const ActivityCalendar = dynamic(
+  () => import('react-activity-calendar'),
+  { ssr: false } // Disable server-side rendering for this component
+);
 
 const shades: Array<[number, string, number]> = [
   [0.25, "#FE8548", 1],
@@ -22,11 +27,15 @@ type Activity = {
 
 
 export default function Home() {
+  const { data: session } = useSession()
   const [dailyData, setDistances] = useState<Activity[]>([]);
   const [error, setError] = useState<string | null>(null);
 
   function binData(data: StravaData[]): Activity[] {
     const bins = data.reduce((acc, d) => {
+      if (d.distance === 0)
+        return acc;
+
       const date = new Date(d.start_date).toISOString().split('T')[0];
       if (!acc[date]) {
         acc[date] = 0;
@@ -38,15 +47,12 @@ export default function Home() {
 
     const dailyData: Activity[] = [];
     const maxDistance = Math.max(...Object.values(bins))
-    console.log(maxDistance);
     Object.keys(bins).forEach((key) => {
       const distance = bins[key];
       const normalizedDistance = distance / maxDistance;
       for (const [threshold, color, level] of shades) {
         if (normalizedDistance <= threshold) {
-          const newLevel = distance === 0 ? 0 : level;
-          console.log("distance: ", distance, normalizedDistance, newLevel)
-          dailyData.push({"date": key, "count": distance, "level": newLevel});
+          dailyData.push({"date": key, "count": distance, "level": level});
           break;
         }
       }
@@ -55,41 +61,53 @@ export default function Home() {
     return dailyData.reverse();
   }
 
-
-  async function callApi() {
-    fetch("api/hello")
-    .then(response => response.json())
-    .then((data: ApiResponse) => {
-      if ('error' in data) {
-        setError(data.error)
-      } else {
-        const DateDistanceBins = binData(data);
-        setDistances(DateDistanceBins);
-      }
-    }).catch(eror => {
-      console.error('Fetch error:', error);
-      setError('Failed to load data');
-    })
-  }
+  useEffect(() => {
+    if (session) {
+      const before: number = new Date(new Date().getFullYear(), 0, 1).getTime() / 1000;
+      const after: number = new Date(new Date().getFullYear() - 2, 11, 31).getTime() / 1000;
+      const page: number = 1;
+      const perPage: number = 200;
+  
+      fetch(`https://www.strava.com/api/v3/athlete/activities?before=${before}&after=${after}&page=${page}&per_page=${perPage}`, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${session.token.access_token}`
+        }
+      })
+      .then(response => response.json())
+      .then((data: StravaData[]) => {
+          const DateDistanceBins = binData(data);
+          setDistances(DateDistanceBins);
+      }).catch(eror => {
+        console.error('Fetch error:', error);
+        setError('Failed to load data');
+      })
+    }
+  }, [session])
 
   return (
-    <div>
-    <Button/>
-    <div/>
-    <button onClick={() => callApi()}>API</button>
-    <div>
-      <ActivityCalendar data={dailyData} weekStart={1} showWeekdayLabels maxLevel={4}
+  <div className="flex justify-center my-20">
+    <div className="relative">
+      <ActivityCalendar
+        data={dailyData}
+        weekStart={1}
+        showWeekdayLabels
+        maxLevel={4}
+        loading={dailyData.length === 0}
         eventHandlers={{
           onClick: (event) => (activity) => {
             alert(JSON.stringify(activity));
           },
         }}
         theme={{
-          light: ["#383838", "#FFDCCB", "#FFC2A4", "#FF8E55", "#FF5C0A",],
-          dark:  ["#383838", "#FFDCCB", "#FFC2A4", "#FF8E55", "#FF5C0A",]
+          light: ["#383838", "#FFDCCB", "#FFC2A4", "#FF8E55", "#FF5C0A"],
+          dark: ["#383838", "#FFDCCB", "#FFC2A4", "#FF8E55", "#FF5C0A"]
         }}
-      ></ActivityCalendar>
+      />
+      <div className="absolute inset-0 flex justify-center items-center z-10">
+        <Button />
+      </div>
     </div>
-    </div>
+  </div>
   )
 }
